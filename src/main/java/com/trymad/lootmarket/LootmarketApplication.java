6f8c83a701558613dbe17eaa191a4ddbe23588ac
Trymad <dummymail@mail.com>
2024-10-28 17:39:23 +0300

    init project

---
+++ src/main/java/com/trymad/lootmarket/LootmarketApplication.java
@@ -0,0 +1,13 @@
+package com.trymad.lootmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootmarketApplication.class, args);
+	}
+
+}
